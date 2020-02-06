d4f3de6cef8e9402a9d575fefb5b1f77485645cc
dbel431 <dbel431@gmail.com>
2020-02-06 12:58:18 +0530

    change

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ angular.module('todoApp', [])
   .controller('TodoListController', function() {
     var todoList = this;
     todoList.data = new Date().getTime();
+    todoList.flag = true;
+    setInterval(function(){
+      todoList.flag = (new Date().getTime()%3 == 0);
+      console.log(todoList.flag);
+  },1000);
+  todoList.get =function(){
+     return todoList.flag;
+  }
+   
   });

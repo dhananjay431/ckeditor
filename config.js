9c688538027aca274523aa553f6ae6ea7cb3afb6
dbel431 <dbel431@gmail.com>
2020-02-06 14:45:46 +0530

    asdf

--- config.js
+++ config.js
@@ -3,8 +3,3 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};

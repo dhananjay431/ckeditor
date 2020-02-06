bbf0e76918241beba3cce8d46a9b25a590fc5429
dbel431 <dbel431@gmail.com>
2020-02-06 11:39:39 +0530

    init

---
+++ samples/js/sample.js
@@ -0,0 +1,28 @@
+﻿
+
+var initSample = function () {
+	if (CKEDITOR.env.ie && CKEDITOR.env.version < 9)
+	CKEDITOR.tools.enableHtml5Elements(document);
+	CKEDITOR.config.height = 150;
+	CKEDITOR.config.width = 'auto';
+
+	var wysiwygareaAvailable = isWysiwygareaAvailable(),
+		isBBCodeBuiltIn = !!CKEDITOR.plugins.get('bbcode');
+	function isWysiwygareaAvailable() {
+		if (CKEDITOR.revision == ('%RE' + 'V%')) {
+			return true;
+		}
+		return !!CKEDITOR.plugins.get('wysiwygarea');
+	}
+
+	var editorElement = CKEDITOR.document.getById('editor');
+	if (isBBCodeBuiltIn) {
+		editorElement.setHtml("");
+	}
+	if (wysiwygareaAvailable) {
+		CKEDITOR.replace('editor');
+	} else {
+		editorElement.setAttribute('contenteditable', 'true');
+		CKEDITOR.inline('editor');
+	}
+}
